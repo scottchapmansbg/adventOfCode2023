7bd1e844e1ba4e35cdf46344bc16b8168a0da6d0
Scott Chapman <scott.chapman@skybettingandgaming.com>
2023-12-08 11:23:58 +0000

    Day1Part1

--- src/main/kotlin/Day1/NumberExtractor.kt
+++ src/main/kotlin/Day1/NumberExtractor.kt
@@ -2,19 +2,66 @@ package Day1
 
 class NumberExtractor {
     fun extractNumbers(data: List<String>): List<Int> {
-        val numbers = data.map { findIntegers(it) }
+        val numbers = data.map { it.findIntegers() }
         val rowOutput = numbers.map { it.first().toString() + it.last().toString() }
-        return rowOutput.map { it.toInt() }
+        return rowOutput.map { Integer.parseInt(it) }
     }
 
-    private fun findIntegers(input:String):List<String>{
+    fun extractNumbersIncludeWords(data: List<String>): List<Int> {
+        val numbers = data.map { it.findIntegersFromWordsAndNumbers() }
+        val rowOutput = numbers.map { it.first().toString() + it.last().toString() }
+        return rowOutput.map { Integer.parseInt(it) }
+    }
+
+    private fun String.findIntegers():List<String>{
         val numbers = mutableListOf<String>()
         val regex = Regex("[0-9]")
-        val matches = regex.findAll(input)
+        val matches = regex.findAll(this)
         matches.forEach {
             numbers.add(it.value)
         }
         return numbers
     }
+
+    private fun String.findIntegersFromWordsAndNumbers():List<Int>{
+        val numbers = mutableListOf<Int>()
+        val regex = Regex("[0-9]|one|two|three|four|five|six|seven|eight|nine")
+        val matches = regex.findAll(this)
+        matches.forEach {
+            numbers.add(it.value.toInt())
+        }
+        return numbers
+    }
+
+    private fun String.toInt() : Int{
+        val wordsToInt = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9
+        )
+        val regex = Regex("[0-9]")
+        return if(wordsToInt.keys.contains(this)){
+            wordsToInt[this]!!
+        } else if (regex.matches(this)){
+            Integer.parseInt(this)
+        } else{
+            throw Exception("Not a number")
+        }
+    }
+
+
+//return when(val testString = this){
+//    wordsToInt.keys.contains(testString) -> {
+//        wordsToInt[this]!!
+//    }
+//    regex.matches(this).toString() -> this.toInt()
+//    else -> throw Exception("Not a number")
+
 }
 
